041bb922fa9cb979ad2b82e21cf3db49ad7ab426
mariano3232 <marianomelend@gmail.com>
2022-07-26 17:03:15 -0300

    rr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./Footer.module.css"
-
+import github from "../GitHub.png"
+import linkedin from "../linkedin.png"
+import henry from "../Henry.jpg"
+import { Link } from "react-router-dom";
 export default function Footer(){
 
+    const githubURL="https://github.com/mariano3232"
 
     return(
         <div className={styles.footer}>
-
+            <div className={styles.icons}>
+                <a href="https://github.com/mariano3232" target='_blank'><img src={github} className={styles.icon} /></a>
+                <a href="https://www.linkedin.com/in/mariano-melendez/" target='_blank'><img src={linkedin} className={styles.icon} /></a>
+                <a href="https://www.talent.soyhenry.com/candidate/5476" target='_blank'><img src={henry} className={styles.icon} /></a>
+            </div>
         </div>
     )
 }
